4c0d73b0d3a4c0b8ff842ac4c65c5ebf2ee389ff
Christopher Jones <chrisdjones15@gmail.com>
2014-07-11 17:35:17 -0500

    Source now creates the fc::Event
    In order to allow the Source to set the event number, it was easiest
    to just have the Source create the fc::Event.

--- Modules/src/CountEventsSource.cc
+++ Modules/src/CountEventsSource.cc
@@ -1,9 +1,15 @@
 #include "CountEventsSource.hh"
 #include "Event.hh"
 
-bool fc::CountEventsSource::fillNextEvent( Event& event) {
-  std::unique_ptr<bool> genDataPtr( new bool{_genData} );
-  event.put("genData", std::move(genDataPtr) );
+std::unique_ptr<fc::Event> fc::CountEventsSource::getNextEvent() {
+  const unsigned int eventNumber = _numberOfEventsProcessed++; 
+  if( _numberOfEventsProcessed < _numberOfEventsToProcess) {
+    std::unique_ptr<Event> event{ new Event(eventNumber) };
 
-  return _numberOfEventsProcessed++ < _numberOfEventsToProcess;
+    std::unique_ptr<bool> genDataPtr( new bool{_genData} );
+    event->put("genData", std::move(genDataPtr) );
+    return std::move(event);
+  }
+
+  return std::unique_ptr<Event>();
 }
