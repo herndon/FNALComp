a3f22a354b006e22c9f208f923e0c41318ede54e
matthew herndon <herndon@hep.wisc.edu>
2014-07-13 21:53:22 -0500

    Fix issue with counting in CountEventsSource

--- Modules/src/CountEventsSource.cc
+++ Modules/src/CountEventsSource.cc
@@ -3,7 +3,7 @@
 
 std::unique_ptr<fc::Event> fc::CountEventsSource::getNextEvent() {
   const unsigned int eventNumber = _numberOfEventsProcessed++; 
-  if( _numberOfEventsProcessed < _numberOfEventsToProcess) {
+  if( _numberOfEventsProcessed <= _numberOfEventsToProcess) {
     std::unique_ptr<Event> event{ new Event(eventNumber) };
 
     std::unique_ptr<bool> genDataPtr( new bool{_genData} );
